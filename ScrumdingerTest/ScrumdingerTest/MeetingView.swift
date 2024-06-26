008a01f5500a14672abfef5ea159ab8591cb1772
sykr1313 <167267895+sykr1313@users.noreply.github.com>
2024-06-26 10:34:12 +0900

    swiftUISample파일 생성

---
+++ ScrumdingerTest/ScrumdingerTest/MeetingView.swift
@@ -0,0 +1,47 @@
+//
+//  MeetingView.swift
+//  ScrumdingerTest
+//
+//  Created by 김소연 on 6/23/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.filled")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.filled")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+                .accessibilityLabel("Next speaker")
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
---
+++ ScrumdingerTest/ScrumdingerTest/ScrumdingerTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerTestApp.swift
+//  ScrumdingerTest
+//
+//  Created by 김소연 on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
